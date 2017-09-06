97d082a6b34924ad9499bae460f05c6b4b6afafa
mezcel <mezcel@mail.com>
2017-09-06 04:34:51 -0400

    removed distracting files

--- version 2/script/myScript-storageVars.js
+++
@@ -1,9 +0,0 @@
-var mycontactObject = {firstName: "", middleName: "", lastName: "", address: "", city: "", state: "", zip: "", phone: "", email: "", myUrl: ""};
-var companyInfoObject = {jobname: "", jobid: "", hours: "", name: "", address: "", city: "", state: "", zip: "", website: "", attn: "", attnemail: "", phone:"", situation:""};
-var industryThemesObject = {companydescription: "", companyphilosophy: "", companycustomers: "", companydistinguish: "" };
-var whatIofferObject = {applicationidentity: "", skillarray: "", knowledgearray: "", abilityarray: ""};
-
-var emailletterObject = {lead:"", research:"", letter:""}; //unused in this version
-var resumeObject = {template1:"", template2:"", template3:""}; //unused in this version
-var coverletterObject = {template1:"", template2:"", template3:""}; //unused in this version
-var powerButton = false;
